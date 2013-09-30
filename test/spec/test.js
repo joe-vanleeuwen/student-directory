47ed08fc1f3e6d8080a1526cd29b15b4e9679310
Joe VanLeeuwen <jvnlwn@gmail.com>
2013-09-30 00:40:08 -0400

    tests not passing. Can't get them passing due to some 569JNORTXZghilmsclick issue. Should pass if I could force an automatic button click.

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,41 @@
-/*global describe, it */
 'use strict';
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('Directory', function () {
 
-            });
-        });
+    	it('should add new student', function() {
+    		$('.add').click();
+
+			$('.student-name').val('Brady Moller');
+			$('.student-cell').val('864-884-5555');
+			$('.student-email').val('bradyMoller@gmail.com');
+			$('.student-github').val('https://github.com/bradyMoller');
+			$('.student-image').val('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQtR5BkM7w6FXDn4xmNem0hckzTkYuopVAeIR6As9UgatIyVwEySQ');
+
+			$('.save').click();
+			var lastStudent = $('.template-container .table-view .name').last();
+			expect(lastStudent.text()).to.equal('Brady Moller');
+    	})
+
+    	it('should edit a student', function() {
+    		var lastStudent = $('.template-container .table-view .name').last();
+    		lastStudent.click();
+
+    		$('.student-name').val('Amy Moller');
+			$('.save').click();
+			$('.go-back-button').click();
+
+			var lastStudent = $('.template-container .table-view .name').last();
+			expect(lastStudent.text()).to.equal('Amy Moller');
+    	})
+
+    	it('should remove a student', function() {
+    		var lastStudent = $('.template-container .table-view .name').last();
+    		lastStudent.click();
+
+			$('.remove').click();
+
+			var lastStudent = $('.template-container .table-view .name').last();
+			expect(lastStudent.text()).to.not.equal('Amy Moller');
+    	})
     });
 })();
